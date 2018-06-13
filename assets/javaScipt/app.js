b68b38ba83639e9e5cf88705ebf926a75ada0317
Katharine Arnold <katiekhs123@gmail.com>
2018-06-13 13:10:15 -0600

    added folders

---
+++ assets/javaScipt/app.js
@@ -0,0 +1,33 @@
+$(() => {
+
+    // quiz questions and answers
+
+
+    // start button
+
+
+    // win screen
+
+
+    //lose screen
+
+
+    // ran out of time screen
+
+
+    // auto start next question after win/lose/ran out of time 4 seconds 
+
+
+    // timer for questions
+
+
+    // final screen, show the number of correct answers, incorrect answers, 
+    
+    
+    //  option to restart the game button (without reloading the page).
+
+
+
+
+
+});
